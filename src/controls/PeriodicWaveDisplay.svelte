197b2ea36bf3ce73ed8caa9c172b696f2cf9c056
Khôi Tran <kt@compile.ch>
2021-01-23 11:45:10 +0100

    periodiccu displayu

---
+++ src/controls/PeriodicWaveDisplay.svelte
@@ -0,0 +1,22 @@
+<script>
+  import {line, scaleLinear, extent} from 'd3';
+
+  export let real: Array<number> = [0,1];
+  export let imag: Array<number> = [0,0];
+
+  const scaleX = scaleLinear()
+  .domain([0, Math.PI * 2])
+    .range([0,240]);
+    
+
+  const scaleY = scaleLinear()
+    .domain(extent([...real, ...imag]))
+    .range([0,100]);
+    
+
+  const lineDrawer = line()
+    .x((d, i) => scaleX(i))
+    .y((d) => scaleY(d));
+  
+
+</script>
