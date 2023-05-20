72d45f97e04368ddb8d196d7158e3ddb2bec450d
Khôi Tran <kt@compile.ch>
2023-05-20 21:31:53 +0200

    file upload dialog

--- src/lib/Components/MidiPlayer/MidiPlayer.svelte
+++ src/lib/Components/MidiPlayer/MidiPlayer.svelte
@@ -9,7 +9,9 @@
     function filedrop(el: Element) {
         const drop = async (ev: DragEvent) => {
             ev.preventDefault();
-            const buf = await ev.dataTransfer.items[0].getAsFile().arrayBuffer();
+            const buf = await ev.dataTransfer.items[0]
+                .getAsFile()
+                .arrayBuffer();
             config.load(buf);
         };
         el.addEventListener("drop", drop);
@@ -18,13 +20,33 @@
             destroy() {},
         };
     }
+
+    async function fileupload(ev) {
+        console.log(ev, ev.target.files);
+        const buf = await new Blob([ev.target.files[0]]).arrayBuffer();
+        config.load(buf);
+    }
+
+    let fileinput: HTMLInputElement;
 </script>
 
 {#if $view == View.FRONT}
     <svg viewBox="0 0 960 250" preserveAspectRatio="xMidYMid meet">
-        <rect width="960" height="250" fill="grey" use:filedrop />
+        <rect
+            width="960"
+            height="250"
+            fill="grey"
+            use:filedrop
+            on:click={() => fileinput.dispatchEvent(new MouseEvent("click"))}
+        />
         <text x="100" y="100">MidiPlayer</text>
     </svg>
+    <input
+        style="display: none"
+        type="file"
+        bind:this={fileinput}
+        on:input={(ev) => fileupload(ev)}
+    />
 {/if}
 
 {#if $view == View.BACK}
