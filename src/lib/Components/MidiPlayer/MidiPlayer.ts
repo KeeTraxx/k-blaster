ce4aa60c069e5ce05131687cb5782510ae3c238b
Khôi Tran <kt@compile.ch>
2023-05-22 07:48:36 +0200

    stuff

--- src/lib/Components/MidiPlayer/MidiPlayer.ts
+++ src/lib/Components/MidiPlayer/MidiPlayer.ts
@@ -2,15 +2,15 @@ import Immutable from "immutable";
 import { writable } from "svelte/store";
 import { Component } from "../Component";
 import { PortDirection, type AudioPort, type MidiPort } from "../types.d";
-import { read } from "midifile-ts";
+import { read, type MidiFile, type AnyEvent } from "midifile-ts";
 
 export class MidiPlayer extends Component {
     public readonly midiPorts: Immutable.Set<MidiPort>;
     public readonly type: string = "MidiPlayer";
     public readonly audioPorts: Immutable.Set<AudioPort>;
-    public ppq = writable(480);
-
     private midiOut: MidiPort;
+
+    private midiFile: MidiFile;
 
     constructor(public readonly id: string) {
         super();
@@ -25,10 +25,35 @@ export class MidiPlayer extends Component {
     }
 
     public load(buffer: ArrayBuffer) {
-        const mididata = read(buffer);
-        console.log(mididata);
+        this.midiFile = read(buffer);
     }
 
     public play() {
+        if (!this.midiFile) {
+            throw new Error("No MIDI File loaded.");
+        }
+
+        this.midiFile.tracks.forEach(trackEvents => {
+            this.playTrack(trackEvents);
+        });
+    }
+
+    private async playTrack(trackEvents: Array<AnyEvent>) {
+        while (trackEvents.length > 0) {
+            const event = trackEvents.shift();
+            if (event.deltaTime > 0) {
+                await this.wait(event.deltaTime);
+            }
+
+            this.emit(event);
+        }
+    }
+
+    private async wait(ms: number) {
+        return await new Promise((resolve) => { setTimeout(() => resolve(undefined), ms) })
+    }
+
+    private emit(ev: AnyEvent) {
+
     }
 }
