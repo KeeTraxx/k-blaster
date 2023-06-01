462274389b3beb7cbfaf1128317dfe67ddb1e692
Khôi Tran <kt@compile.ch>
2023-06-01 10:08:10 +0200

    disconnect and connect working

--- src/lib/Components/HardwareIO/HardwareIO.ts
+++ src/lib/Components/HardwareIO/HardwareIO.ts
@@ -1,7 +1,7 @@
 import Immutable from "immutable";
 import { Component } from "../Component";
 import { type AudioPort, PortDirection, type MidiPort, type MidiDevice } from "../types";
-import {disconnect} from './../../Helper/midiport';
+import { disconnect } from './../../Helper/midiport';
 
 export class HardwareIO extends Component {
     public readonly type: string = "HardwareIO";
@@ -33,31 +33,62 @@ export class HardwareIO extends Component {
     }
 
     private updateMidiDevices() {
-        [...this.midiDevices.values()].forEach(dev => {
-            disconnect(dev.input)
-            disconnect(dev.output)
+        console.log('update midi devices');
+        
+        const newDeviceNames = [
+            ...this.midiAccess.inputs.values(),
+            ...this.midiAccess.outputs.values()
+        ].map(dev => this.getName(dev));
+        
+        [...this.midiDevices.values()].forEach(midiDevice => {
+            const name = this.getName(midiDevice);
+            console.log('checking ', name, newDeviceNames);
+            if (!newDeviceNames.includes(name)) {
+                // remove disconnected devices
+                console.log(`${name} has disconnected`);
+                disconnect(midiDevice.input)
+                disconnect(midiDevice.output)
+                this.midiDevices.delete(name);
+            }
         });
-        this.midiDevices.clear();
+        
+
+        // add new devices
         [
             ...this.midiAccess.inputs.values(),
             ...this.midiAccess.outputs.values()
-        ].forEach(dev => this.midiDevices.set(this.getName(dev), {manufacturer: dev.manufacturer, name: dev.name}));
-
-        [...this.midiAccess.inputs.values()].forEach(dev => this.midiDevices.get(this.getName(dev)).input = {
-            componentId: this.id,
-            direction: PortDirection.OUT,
-            midi: dev,
-            name: `${this.getName(dev)}_OUT`
+        ].forEach(dev => {
+            const name = this.getName(dev);
+            if (!this.midiDevices.has(name))
+                this.midiDevices.set(name, { manufacturer: dev.manufacturer, name: dev.name });
         });
-        [...this.midiAccess.outputs.values()].forEach(dev => this.midiDevices.get(this.getName(dev)).output = {
-            componentId: this.id,
-            direction: PortDirection.IN,
-            midi: dev,
-            name: `${this.getName(dev)}_IN`
+
+        [...this.midiAccess.inputs.values()].forEach(dev => {
+            const midiDevice = this.midiDevices.get(this.getName(dev));
+            if (!midiDevice.input) {
+                midiDevice.input = {
+                    componentId: this.id,
+                    direction: PortDirection.OUT,
+                    midi: dev,
+                    name: `${this.getName(dev)}_OUT`
+                };
+            }
+        });
+
+        [...this.midiAccess.outputs.values()].forEach(dev => {
+            const midiDevice = this.midiDevices.get(this.getName(dev));
+            if (!midiDevice.output) {
+                midiDevice.output = {
+                    componentId: this.id,
+                    direction: PortDirection.IN,
+                    midi: dev,
+                    name: `${this.getName(dev)}_IN`
+                };
+            }
         });
     }
 
-    private getName(midiDevice: MIDIInput | MIDIOutput) : string {
+    private getName(midiDevice: MIDIInput | MIDIOutput | MidiDevice): string {
         return `${midiDevice.manufacturer}_${midiDevice.name}`;
     }
 }
